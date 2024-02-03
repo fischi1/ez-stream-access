3c00d9a7480a2fe4d18ca6f7913520f97066ef10
Lukas <fischi.ftw@gmail.com>
2024-02-04 00:12:23 +0100

    loading indicator

--- popup/components/control/Control.tsx
+++ popup/components/control/Control.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import browser from "webextension-polyfill"
 import { Message } from "../../../background"
 import img from "../../../public/icon-72x72.png"
+import { useAppState } from "../../state/StateContext"
 import Menu from "./Menu"
 import QualityControls from "./QualityControls"
 
 type Props = {}
 
 const Control = ({}: Props) => {
+    const state = useAppState()
+
     const handleRefreshClick = async () => {
         browser.runtime.sendMessage({
             action: "refreshStreams",
@@ -29,8 +32,12 @@ const Control = ({}: Props) => {
                 <QualityControls />
                 <div>
                     <button
-                        className="hover:bg-background rounded-full p-1"
+                        className="enabled:hover:bg-background rounded-full p-1 enabled:text-typography disabled:text-slate-300"
                         onClick={handleRefreshClick}
+                        disabled={
+                            state.loggedInState.status !== "LOGGED_IN" ||
+                            state.streamState.status === "FETCHING"
+                        }
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -38,12 +45,14 @@ const Control = ({}: Props) => {
                             height="25"
                             fill="currentColor"
                             viewBox="0 0 16 16"
+                            className={
+                                state.streamState.status === "FETCHING"
+                                    ? "animate-spin"
+                                    : undefined
+                            }
                         >
                             {/* https://icons.getbootstrap.com/icons/arrow-clockwise/ */}
-                            <path
-                                fill-rule="evenodd"
-                                d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
-                            />
+                            <path d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
                             <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
                         </svg>
                     </button>
